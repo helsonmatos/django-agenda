0359c410113b4c4bccc033c5a44da8ccf2e55ae8
Helson Matos <77506662+helsonmatos@users.noreply.github.com>
2022-05-23 10:22:14 -0300

    campo de pesquisa
    campo de pesquisa muito complexo para aprender, mas é show demais

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Value, Q
+from django.db.models.functions import Concat
 
 # Create your views here.
 def index(request):
@@ -25,4 +27,26 @@ def detalhes(request,contato_id):
         raise Http404()
     return render(request, 'contatos/detalhes.html',{
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+            nome_completo = campos
+        ).filter(
+            Q(nome_completo__icontains = termo) | Q(telefone__icontains = termo)
+    )
+    paginator = Paginator(contatos,5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html',{
+        'contatos': contatos
     })
