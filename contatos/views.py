955050ab49d3bdd37ebcb3bd0f0ef4ee41faf986
Helson Matos <77506662+helsonmatos@users.noreply.github.com>
2022-05-23 09:00:44 -0300

    paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos,20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
     return render(request, 'contatos/index.html',{
         'contatos': contatos
     })
